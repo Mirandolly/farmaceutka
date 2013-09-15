229fd37474b67816da13bdd53ac39710c5319a54
Miroslav Petrik <miroslavpetrik@outlook.com>
2013-09-15 15:57:29 +0200

    Jekyll

---
+++ _sass/config.rb
@@ -0,0 +1,11 @@
+require 'animation'
+require 'breakpoint'
+
+http_path = "/"
+sass_dir = "./"
+css_dir = "../css"
+images_dir = "../img"
+javascripts_dir = "../js"
+relative_assets = true
+line_comments = false
+output_style = :compressed
